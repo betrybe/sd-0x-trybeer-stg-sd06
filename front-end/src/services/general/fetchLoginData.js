e040695de874f98ec60103e0e3839d8e1da11909
Renato Filho <renatosousafilho@gmail.com>
2021-03-11 08:33:35 -0300

    linter

--- front-end/src/services/general/fetchLoginData.js
+++ front-end/src/services/general/fetchLoginData.js
@@ -1,15 +1,13 @@
 const URL = 'http://localhost:3001/login';
 
-const fetchUserData = async (userData) => {
-  return await fetch(URL, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  })
+const fetchUserData = (userData) => fetch(URL, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(userData),
+})
   .then((response) => response.json());
-};
 
 export default fetchUserData;
